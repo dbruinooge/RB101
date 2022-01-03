a62510f1bc588edc1df35cb0f1fd6c1cd95f9c9e
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-01-03 01:30:38 +0000

    Add features to twenty_one.rb

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -2,7 +2,18 @@ CARD_VALUES = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
                 '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'Jack' => 10,
                 'Queen' => 10, 'King' => 10, 'Ace' => 11 }
 
-# add constant LIMMIT = 21
+LIMIT = 21
+DEALER_STANDS = 17
+
+def joinor(arr, delimiter=', ', word='and')
+  if arr.size == 2
+    arr.join(" #{word} ")
+  else
+    cards = arr.dup
+    cards[-1] = "#{word} #{arr.last}"
+    cards.join(delimiter)
+  end
+end
 
 def initialize_deck
   deck = []
@@ -21,6 +32,7 @@ end
 def initialize_dealer(deck)
   dealer_hand = []
   2.times { deal_card!(deck, dealer_hand) }
+  dealer_hand.insert(-2, "unknown card")
   dealer_hand
 end
 
@@ -30,12 +42,60 @@ def deal_card!(deck, hand)
   deck.delete_at(deck.index(card))
 end
 
-def display_game_state(d_hand, p_hand)
-  puts "Dealer has: #{d_hand[0]} and unknown card"
-  puts "Player has: #{p_hand}"
+def hit?
+  loop do
+    puts "Hit? (y/n)"
+    choice = gets.chomp.downcase
+    return true if choice.start_with?('y')
+    return false if choice.start_with?('n')
+    puts "Invalid input."
+  end
 end
 
-def calculate_score(hand)
+def player_takes_turn!(deck, d_hand, p_hand)
+  display_game_state(d_hand, p_hand)
+  while hit?
+    deal_card!(deck, p_hand)
+    display_game_state(d_hand, p_hand)
+    break if busted?(p_hand)
+  end
+end
+
+def dealer_takes_turn!(deck, d_hand, p_hand)
+  d_hand.delete('unknown card')
+  display_game_state(d_hand, p_hand)
+  while score(d_hand) < DEALER_STANDS
+    deal_card!(deck, d_hand)
+    display_game_state(d_hand, p_hand)
+    puts "Dealer draws #{d_hand[-1]}. Press ENTER to continue.."
+    gets
+  end
+end
+
+def display_game_state(d_hand, p_hand)
+  system 'clear'
+  display_dealer(d_hand)
+  puts "-----------------------------------------"
+  display_player(p_hand)
+  puts "========================================="
+end
+
+def display_dealer(d_hand)
+  if d_hand.any?('unknown card')
+    puts "Dealer has: #{joinor(d_hand[0..1])}"
+    puts "Dealer score: #{CARD_VALUES[d_hand[0]]} + ?"
+  else
+    puts "Dealer has: #{joinor(d_hand)}"
+    puts "Dealer score: #{score(d_hand)}"
+  end
+end
+
+def display_player(p_hand)
+  puts "Player has: #{joinor(p_hand)}"
+  puts "Player score: #{score(p_hand)}"
+end
+
+def score(hand)
   score = 0
   hand.each { |card| score += CARD_VALUES[card] }
   aces = hand.count('Ace')
@@ -47,23 +107,43 @@ def calculate_score(hand)
 end
 
 def busted?(hand)
-  calculate_score(hand) > 21
+  score(hand) > LIMIT
 end
 
-deck = initialize_deck
-player_hand = initialize_player(deck)
-dealer_hand = initialize_dealer(deck)
+def result(d_hand, p_hand)
+  if busted?(p_hand)
+    "Player busted!"
+  elsif busted?(d_hand)
+    "Dealer busted!"
+  elsif score(p_hand) > score(d_hand)
+    "Player wins!"
+  elsif score(d_hand) > score(p_hand)
+    "Dealer wins!"
+  else
+    "It's a tie!"
+  end
+end
 
-loop do
-  # display method, player turn method, dealer turn method
-  system 'clear'
-  display_game_state(dealer_hand, player_hand)
-  puts "Player score: #{calculate_score(player_hand)}"
-  break if busted?(player_hand)
-  puts "Hit?"
+def again?
+  puts "Do you want to play again? (y/n)"
   choice = gets.chomp
-  break if choice == 'n'
-  deal_card!(deck, player_hand)
+  return true if choice.downcase.start_with?('y')
+  false
 end
 
-puts "Player busted!" if busted?(player_hand)
+# gameplay loop
+loop do
+  deck = initialize_deck
+  player_hand = initialize_player(deck)
+  dealer_hand = initialize_dealer(deck)
+
+  player_takes_turn!(deck, dealer_hand, player_hand)
+  dealer_takes_turn!(deck, dealer_hand, player_hand) if !busted?(player_hand)
+
+  display_game_state(dealer_hand, player_hand)
+  puts result(dealer_hand, player_hand)
+  break if !again?
+end
+
+puts "Goodbye!"
+
